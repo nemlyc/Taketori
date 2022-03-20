da243d254ff1918373e5d3147ae18ecc362eb418
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-20 18:44:42 +0900

    add: score sample - add test correction item - usecase

---
+++ Assets/Scripts/Scoring/Samples/SampleScoreManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ScoreManager))]
+public class SampleScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    CorrectionItem[] items;
+
+    ScoreManager scoreManager;
+
+    private void Awake()
+    {
+        scoreManager = GetComponent<ScoreManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<GenericBamboo>(out var bamboo))
+        {
+            var score = scoreManager.currentScore;
+            scoreManager.currentScore = bamboo.CalcScore(score);
+
+            bamboo.AttackAction();
+            var type = bamboo.GetBambooType();
+            scoreManager.AddBamboo(type);
+
+            Debug.Log(scoreManager.currentScore);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        var entity = scoreManager.FetchResult();
+        Debug.Log(entity);
+    }
+
+    void ObtainTestItem()
+    {
+        foreach (var item in items)
+        {
+            ItemEntity entity = new ItemEntity()
+            {
+                IsGot = true
+            };
+            entity.SetID(item.GetID());
+
+            scoreManager.AddItem(entity);
+        }
+
+        
+    }
+}
