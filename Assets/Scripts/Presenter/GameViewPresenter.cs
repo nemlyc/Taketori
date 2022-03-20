ca1195c51e304bd6614ab2a10f35ad4a15ab8125
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-20 23:20:46 +0900

    add: インゲームシーンを仮作成

---
+++ Assets/Scripts/Presenter/GameViewPresenter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class GameViewPresenter : MonoBehaviour
+{
+    /*
+     * Todo : Age
+     * 
+     * 購読する
+     *  - Timer
+     *  - Age
+     *  - Score
+     * 
+     * UIを書き換える
+     *  - Timer Value
+     *  - Age Value
+     *  - Score Value
+     */
+
+    [SerializeField]
+    ScoreManager score;
+    [SerializeField]
+    Timer timer;
+    [SerializeField]
+    InGameView gameView;
+
+    private void Start()
+    {
+        timer.currentTime.Subscribe(time =>
+        {
+            gameView.UpdateTimerValue(time);
+        }).AddTo(this);
+
+        timer.IsTimeUp.Subscribe(timeUp =>
+        {
+            Debug.Log("タイムアップ！ wip");
+        }).AddTo(this);
+
+        score.currentScore.Subscribe(score =>
+        {
+            gameView.UpdateScoreValue(score);
+        }).AddTo(this);
+    }
+}
---
+++ Assets/Scripts/View/InGameView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InGameView : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text ageValue, scoreValue, timerValue;
+
+    //GameViewPresenter presenter;
+
+    public void UpdateAgeValue(int age)
+    {
+        ageValue.text = age.ToString();
+    }
+
+    public void UpdateScoreValue(int score)
+    {
+        scoreValue.text = score.ToString();
+    }
+
+    public void UpdateTimerValue(string time)
+    {
+        timerValue.text = time;
+    }
+}
