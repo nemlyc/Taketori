87d27554ad699f10abafbd1182db401f13a4fe31
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-20 18:43:48 +0900

    add: score calcuration script - add Entity - add score manager

---
+++ Assets/Scripts/Entity/CorrectionItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/CorrectionItem")]
+public class CorrectionItem : ScriptableObject
+{
+    [SerializeField]
+    private string ID;
+    [SerializeField]
+    private Sprite sprite;
+
+    public string GetID()
+    {
+        return ID;
+    }
+
+    public Sprite GetSprite()
+    {
+        return sprite;
+    }
+
+    public CorrectionItem()
+    {
+        ID = System.Guid.NewGuid().ToString();
+    }
+}
---
+++ Assets/Scripts/Scoring/ScoreManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int currentScore { get; set; }
+
+    ScoreEntity currentEntity;
+
+    public void AddBamboo(BambooInfo.BambooType type)
+    {
+        if (currentEntity == null)
+        {
+            currentEntity = new ScoreEntity();
+        }
+
+        switch (type)
+        {
+            case BambooInfo.BambooType.Normal:
+                currentEntity.NormalNum += 1;
+                break;
+
+            case BambooInfo.BambooType.Shine:
+                currentEntity.ShineNum += 1;
+                break;
+
+            case BambooInfo.BambooType.Kaguya:
+                currentEntity.KaguyaNum += 1;
+                break;
+        }
+    }
+    public void AddItem(ItemEntity item)
+    {
+        if (currentEntity == null)
+        {
+            currentEntity = new ScoreEntity();
+        }
+
+        currentEntity.itemEntities.Add(item);
+    }
+
+    public ScoreEntity FetchResult(bool clearTempScore = true)
+    {
+        var result = currentEntity;
+        if (clearTempScore)
+        {
+            currentEntity = null;
+        }
+
+        return result;
+    }
+
+    public ScoreEntity LoadLastHighScore(out string status)
+    {
+        status = "記録なし";
+        return new ScoreEntity();
+    }
+
+    public bool WriteItems(out string status)
+    {
+        status = "未実装";
+        return true;
+    }
+
+    public bool WriteHighScore(out string status)
+    {
+        status = "未実装";
+        return true;
+    }    
+}
