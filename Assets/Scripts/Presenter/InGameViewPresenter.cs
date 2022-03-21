2a309d0a6713f104434377c86a538120ea5b93f5
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-21 15:55:07 +0900

    renamed script

--- Assets/Scripts/Presenter/InGameViewPresenter.cs
+++ Assets/Scripts/Presenter/InGameViewPresenter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 
-public class GameViewPresenter : MonoBehaviour
+public class InGameViewPresenter : MonoBehaviour
 {
     /*
      * Todo : Age
