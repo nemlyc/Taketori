8e1637cdc39ae1fe5d85826b04589eafcf68c607
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-21 17:54:52 +0900

    add sample: - file management system

---
+++ Assets/Scripts/Util/Test/DataRWTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class DataRWTest : MonoBehaviour
+{
+    [SerializeField]
+    CollectionItem item1;
+
+    private void Start()
+    {
+        ItemEntity i1 = new ItemEntity();
+        i1.ID = item1.GetID();
+        i1.IsGot = true;
+        Debug.Log(i1.ID);
+        List<ItemEntity> items = new List<ItemEntity>();
+        items.Add(i1);
+
+        ScoreEntity score = new ScoreEntity()
+        {
+            Date = System.DateTime.Now,
+            NormalNum = 11,
+            ShineNum = 5,
+            KaguyaNum = 1,
+            itemEntities = items
+        };
+
+        PlayerDataManager.WriteProgressData(items);
+        PlayerDataManager.WriteScoreData(score);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (PlayerDataManager.LoadProgressData(out var a))
+            {
+                Debug.Log(a[0].ToString());
+            }
+            if (PlayerDataManager.LoadScoreData(out var b))
+            {
+                Debug.Log(b.ToString());
+            }
+        }
+    }
+}
