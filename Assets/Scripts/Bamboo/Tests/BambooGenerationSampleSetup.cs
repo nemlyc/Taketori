9d2ad8c4a6d6b1d56ef043d197e0805bbb5f9e0b
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-23 01:42:08 +0900

    update: 竹のセットアップアルゴリズムを変更

---
+++ Assets/Scripts/Bamboo/Tests/BambooGenerationSampleSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BambooGenerationSampleSetup : MonoBehaviour
+{
+    [SerializeField]
+    BambooGenerator gen;
+
+    private void Start()
+    {
+        gen.PlacementBamboo();
+    }
+}
