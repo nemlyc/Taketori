3a7bc827403870e89e5470644065e05a90048de6
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-25 00:33:26 +0900

    add: コレクションビューの追加 - ウインドウの追加 - ホームビューにコンポーネントを追加

---
+++ Assets/Scripts/Presenter/CollectionViewPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UniRx;
+
+public class CollectionViewPresenter : MonoBehaviour
+{
+    [SerializeField]
+    CollectionView collectionView;
+
+    [SerializeField]
+    HomeViewPresenter homeViewPresenter;
+
+    public List<ItemEntity> GetItems()
+    {
+        return homeViewPresenter.items;
+    }
+}
