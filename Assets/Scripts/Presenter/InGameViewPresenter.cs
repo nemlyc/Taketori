ab0954015b6c9fc775ec2dca66f588450327b901
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-27 18:48:17 +0900

    merge result view to InGame Scene

--- Assets/Scripts/Presenter/InGameViewPresenter.cs
+++ Assets/Scripts/Presenter/InGameViewPresenter.cs
@@ -30,6 +30,12 @@ public class InGameViewPresenter : MonoBehaviour
     [SerializeField]
     float readyTime = 4, ingameTime = 300;
 
+    [SerializeField]
+    ResultViewPresenter resultViewPresenter;
+
+    [SerializeField]
+    CollectionItem[] testItem;
+
     private void Start()
     {
         GameInitialize();
@@ -79,7 +85,7 @@ public class InGameViewPresenter : MonoBehaviour
         score.currentScore.Subscribe(score =>
         {
             gameView.UpdateScoreValue(score);
-        }).AddTo(this);
+        }).AddTo(this);      
     }
 
     void GameInitialize()
@@ -105,8 +111,9 @@ public class InGameViewPresenter : MonoBehaviour
         Time.timeScale = 0;
 
         SaveHighScore(score.currentEntity);
+        SaveItemProgress(score.currentEntity.itemEntities);
 
-        // Todo: Show Result Window
+        resultViewPresenter.ShowResultView(true);
     }
 
     #region LocalMethod
@@ -119,5 +126,25 @@ public class InGameViewPresenter : MonoBehaviour
             PlayerDataManager.WriteScoreData(thisScore);
         }
     }
+    void SaveItemProgress(List<ItemEntity> items)
+    {
+        PlayerDataManager.WriteProgressData(items);
+    }
     #endregion
+
+    void TestScore()
+    {
+        score.AddBamboo(BambooInfo.BambooType.Normal);
+        score.AddBamboo(BambooInfo.BambooType.Normal);
+        score.AddBamboo(BambooInfo.BambooType.Shine);
+        score.AddBamboo(BambooInfo.BambooType.Normal);
+        score.AddBamboo(BambooInfo.BambooType.Kaguya);
+        foreach (var i in testItem)
+        {
+            var ent = new ItemEntity();
+            ent.ID = i.GetID();
+            ent.IsGot = true;
+            score.AddItem(ent);
+        }
+    }
 }
