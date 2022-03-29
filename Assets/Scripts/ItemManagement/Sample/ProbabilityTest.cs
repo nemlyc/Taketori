163b1c25f425f86e38835125cb88609a02ad7593
ishide_motoki <cf.m1t0k@gmail.com>
2022-03-30 01:51:00 +0900

    add: アイテムの抽選スクリプト

---
+++ Assets/Scripts/ItemManagement/Sample/ProbabilityTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbabilityTest : MonoBehaviour
+{
+    [SerializeField]
+    ItemManager item;
+
+    private void Awake()
+    {
+        int i = 0;
+        float atari = 0;
+        float max = 1000000;
+
+        while (i < max)
+        {
+            if (item.PickUp() != -1)
+            {
+                atari++;
+            }
+            i++;
+        }
+
+        Debug.Log($"あたりの数は [{atari}]。確率は {atari / max * 100} %");
+    }
+}
