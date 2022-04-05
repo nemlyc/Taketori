d22902561ac4eb7af188ac742f5b6a8258e0d961
ishide_motoki <cf.m1t0k@gmail.com>
2022-04-05 23:37:51 +0900

    add audio player scripts

---
+++ Assets/Scripts/Audio/AudioMapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMapper : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip HomeBGM, InGameBGM, ResultBGM;
+    [SerializeField]
+    AudioClip NormalAttack, SpecialAttack;
+    [SerializeField]
+    AudioClip HitNormal, HitKaguya, GetItem;
+    [SerializeField]
+    AudioClip UIClick;
+
+    public Dictionary<string, AudioClip> audioMap = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        audioMap.Add(AudioInfo.HomeBGM, HomeBGM);
+        audioMap.Add(AudioInfo.InGameBGM, InGameBGM);
+        audioMap.Add(AudioInfo.ResultBGM, ResultBGM);
+
+        audioMap.Add(AudioInfo.NormalAttack, NormalAttack);
+        audioMap.Add(AudioInfo.SpecialAttack, SpecialAttack);
+
+        audioMap.Add(AudioInfo.HitNormal, HitNormal);
+        audioMap.Add(AudioInfo.HitKaguya, HitKaguya);
+
+        audioMap.Add(AudioInfo.GetItem, GetItem);
+
+        audioMap.Add(AudioInfo.UIClick, UIClick);
+    }
+}
---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioMapper))]
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    /*
+     * ID<場面の名前>を指定して音を再生する。
+     * BGMと効果音は分ける。
+     */
+
+    [SerializeField]
+    AudioMapper mapper;
+    [SerializeField]
+    AudioSource bgmSource, seSource;
+    [SerializeField]
+    float MasterVolume = 1f;
+    [SerializeField]
+    float BGMVolume = 0.03f;
+
+    public void PlayBGMFromMap(string name)
+    {
+        if (TryGetClip(name, out var clip))
+        {
+            bgmSource.clip = clip;
+            bgmSource.Play();
+        }
+    }
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+    }
+
+    public void PlayOneShot(string name)
+    {
+        if (TryGetClip(name, out var data))
+        {
+            seSource.PlayOneShot(data);
+        }
+    }
+
+    public void PlayBambooSE(BambooInfo.BambooType type)
+    {
+        switch (type)
+        {
+            case BambooInfo.BambooType.Normal:
+            case BambooInfo.BambooType.Shine:
+                PlayOneShot(AudioInfo.HitNormal);
+                break;
+            case BambooInfo.BambooType.Kaguya:
+                PlayOneShot(AudioInfo.HitKaguya);
+                break;
+        }
+    }
+
+    bool TryGetClip(string name, out AudioClip clip)
+    {
+        if (mapper.audioMap.TryGetValue(name, out clip))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Awake()
+    {
+        bgmSource.volume = BGMVolume;
+        seSource.volume = MasterVolume;
+    }
+
+    private void Reset()
+    {
+        mapper = GetComponent<AudioMapper>();
+        bgmSource = GetComponent<AudioSource>();
+        seSource = GetComponent<AudioSource>();
+    }
+}
