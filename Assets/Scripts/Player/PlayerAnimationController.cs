fdda5a8e0ee38b029a595e463e8694a44fc335e9
ishide_motoki <cf.m1t0k@gmail.com>
2022-04-06 15:05:49 +0900

    add: player animation

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController
+{
+    Animator animator;
+
+    enum Param
+    {
+        Attack,
+        IsMove,
+    }
+
+    enum State
+    {
+        Attack,
+        Run,
+        Idle
+    }
+
+    readonly int BaseLayer = 0;
+    readonly int UpperLayer = 1;
+    readonly int StartTime = 0;
+
+    public PlayerAnimationController(Animator animator)
+    {
+        this.animator = animator;
+    }
+
+    public void Attack()
+    {
+        animator.Play(State.Attack.ToString(), UpperLayer, StartTime);
+    }
+
+    public void Move()
+    {
+        animator.Play(State.Run.ToString());
+    }
+
+    public void SetMove(bool isMove)
+    {
+        animator.SetBool(Param.IsMove.ToString(), isMove);
+    }
+}
