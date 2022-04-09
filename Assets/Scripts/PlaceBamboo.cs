6671aeb0fe7b13311cbf163d3725bb1614402738
Kou_Miyazaki <miyazaki.magi@outlook.com>
2022-04-09 17:57:56 +0900

    create PlaceBamboo method

--- Assets/Scripts/PlaceBamboo.cs
+++ Assets/Scripts/PlaceBamboo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaceItems : MonoBehaviour
+public class PlaceBamboo : MonoBehaviour
 {
     public GameObject bamboo;
     [SerializeField] Terrain terrain;
@@ -11,6 +11,17 @@ public class PlaceItems : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {        
+        RandomPlaceBamboo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RandomPlaceBamboo()
+    {
         for(int i = 0; i < itemcount; i++)
         {
             float x = Random.Range(0, terrain.terrainData.size.x);
@@ -19,11 +30,5 @@ public class PlaceItems : MonoBehaviour
             Vector3 spawnposi = new Vector3(x, height, z);
             Instantiate(bamboo, spawnposi, transform.rotation);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
